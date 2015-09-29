b183d6521d2e63bbe5c39b0121b1fe658c7934d7
Ryan N. Norris <ryan.norris@my.ccsu.edu>
2015-09-29 11:36:37 -0400

    mt2: Cleanup unused packages.

--- mt2/mt2-vendor.mk
+++ mt2/mt2-vendor.mk
@@ -16,9 +16,6 @@
 
 # Pick up overlay for features that depend on non-open-source files
 PRODUCT_PACKAGES += \
-    com.qualcomm.location \
-    TimeService \
-    qcrilmsgtunnel \
     qcnvitems \
     qcrilhook \
     libHevcSwDecoder
